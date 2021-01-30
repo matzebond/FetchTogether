1eeb0a386336834c0081ecdb14dfe8910cde6b57
Angelo Aracri <angeloaracri@yahoo.de>
2021-01-30 17:39:37 +0100

    Randomly spawn Items into Rooms

---
+++ autoloads/Rand.gd
@@ -0,0 +1,13 @@
+tool
+extends Node
+
+var _rng:RandomNumberGenerator
+
+func _ready():
+    _rng = RandomNumberGenerator.new()
+    _rng.seed = 0
+    
+func randf()->float:
+    return _rng.randf()
+func randi()->int:
+    return _rng.randi()
--- scenes/map/room.gd
+++ scenes/map/room.gd
@@ -7,6 +7,10 @@ enum Orientation {
 
 export(Orientation) var orientation = Orientation.E setget _set_orientation
 
+const ITEM_SCENE_PATH = "res://scenes/items/"
+const ITEM_MAIN_SCENE = "item.tscn"
+var item_scenes = []
+
 func _ready():
     $TileMapOriginal.visible = false
     $TileMapRotated.visible = true
@@ -15,9 +19,21 @@ func _ready():
     if !Engine.editor_hint:
         $TileMapOriginal.queue_free()
     
-    _set_orientation(orientation)
-        
+    # Preload items
+    item_scenes = []
+    var dir = Directory.new()
+    if dir.open(ITEM_SCENE_PATH) == OK:
+        dir.list_dir_begin()
+        var file_name = dir.get_next()
+        while file_name != "":
+            if !dir.current_is_dir() and file_name != ITEM_MAIN_SCENE:
+                item_scenes.append(load(ITEM_SCENE_PATH + file_name))
+            file_name = dir.get_next()
+    else:
+        printerr("Room: Could not iterate " + ITEM_SCENE_PATH)
 
+    _set_orientation(orientation)
+    
 func _set_orientation(v):
     orientation = v
     
@@ -25,19 +41,46 @@ func _set_orientation(v):
     
     var mapOriginal:TileMap = $TileMapOriginal
     var mapRotated:TileMap = $TileMapRotated
+    var mapItemSpawnPoints:TileMap = $TileMapItemSpawnPoints
     
-    mapRotated.clear();
+    var itemRoot:Node2D = $Items
     
+    var cellSize = mapOriginal.cell_size
+    
+    # Clear all previous data
+    mapRotated.clear()
+    for item in itemRoot.get_children():
+        item.queue_free()
+    
+    # Rotate tilemap
     for cell in mapOriginal.get_used_cells():
-        var cell_rotated
-        match orientation:
-            Orientation.N:
-                cell_rotated = Vector2(cell.y, cell.x)
-            Orientation.E:
-                cell_rotated = cell
-            Orientation.S:
-                cell_rotated = Vector2(cell.y, -cell.x)
-            Orientation.W:
-                cell_rotated = Vector2(-cell.x, cell.y)
-        
+        var cell_rotated = rotate_map_pos(cell, orientation)
         mapRotated.set_cellv(cell_rotated, mapOriginal.get_cellv(cell))
+    
+    # Rotate and create spawn points
+    if item_scenes.size() > 0:
+        for cell in mapItemSpawnPoints.get_used_cells():
+            var cell_rotated = rotate_map_pos(cell, orientation)
+            
+            # TODO no duplicates
+            var item_id = Rand.randi() % item_scenes.size()
+            var item = item_scenes[item_id].instance()
+            
+            # Set pos and add to scene
+            item.position = mapRotated.map_to_world(cell_rotated) + cellSize / 2.0
+            itemRoot.add_child(item)
+    else:
+        print("Room: Could not spawn Items into room, there are none loaded")
+
+
+func rotate_map_pos(map_pos:Vector2, orientation):
+    match orientation:
+        Orientation.N:
+            return Vector2(map_pos.y, map_pos.x)
+        Orientation.E:
+            return map_pos
+        Orientation.S:
+            return Vector2(map_pos.y, -map_pos.x)
+        Orientation.W:
+            return Vector2(-map_pos.x, map_pos.y)
+
