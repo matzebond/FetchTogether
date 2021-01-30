02ec80205ba757a8bfb1e54389eaf9e06ece1b55
Angelo Aracri <angeloaracri@yahoo.de>
2021-01-30 10:21:14 +0100

    SPAAAACE

--- scenes/map/rock.gd
+++ scenes/map/rock.gd
@@ -2,10 +2,10 @@ extends KinematicBody2D
 
 # Sent to everyone else
 puppet func do_explosion():
-	$"AnimationPlayer".play("explode")
+    $"AnimationPlayer".play("explode")
 
 # Received by owner of the rock
 master func exploded(by_who):
-	rpc("do_explosion") # Re-sent to puppet rocks
-	$"../../Score".rpc("increase_score", by_who)
-	do_explosion()
+    rpc("do_explosion") # Re-sent to puppet rocks
+    $"../../Score".rpc("increase_score", by_who)
+    do_explosion()
