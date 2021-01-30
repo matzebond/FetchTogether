f633868b15e428db172d7e22b3406ff007f238b0
Angelo Aracri <angeloaracri@yahoo.de>
2021-01-30 10:51:07 +0100

    Fix bugs, add controller bindings

--- autoloads/State.gd
+++ autoloads/State.gd
@@ -72,15 +72,14 @@ func unregister_player(id):
     players.erase(id)
     emit_signal("player_list_changed")
 
-
 remote func pre_start_game(spawn_points):
     # Change scene.
-    var world = load("res://world.tscn").instance()
+    var world = load("res://scenes/screens/world.tscn").instance()
     get_tree().get_root().add_child(world)
 
     get_tree().get_root().get_node("Lobby").hide()
 
-    var player_scene = load("res://player.tscn")
+    var player_scene = load("res://scenes/player/player.tscn")
 
     for p_id in spawn_points:
         var spawn_pos = world.get_node("SpawnPoints/" + str(spawn_points[p_id])).position
