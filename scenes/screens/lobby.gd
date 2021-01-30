13cefb4e40040e9aace3d8aaa1baf0d7feeee7b2
Angelo Aracri <angeloaracri@yahoo.de>
2021-01-30 11:25:10 +0100

    Adjusted lobby

--- scenes/screens/lobby.gd
+++ scenes/screens/lobby.gd
@@ -1,5 +1,20 @@
 extends Control
 
+const ip = "46.232.250.165"
+
+
+onready var panelConnect = $Connect
+onready var panelPlayers = $Players
+
+onready var inputName = $Connect/VBoxContainer/Name
+onready var lblError = $Connect/VBoxContainer/ErrorLabel
+
+onready var btnPlayOnline = $Connect/VBoxContainer/HBoxContainer/BtnPlayOnline
+onready var btnPlayOffline = $Connect/VBoxContainer/HBoxContainer/BtnPlayOffline
+onready var btnStart = $Players/Start
+
+onready var listPlayers = $Players/List
+
 func _ready():
     # Called every time the node is added to the scene.
     State.connect("connection_failed", self, "_on_connection_failed")
@@ -9,84 +24,74 @@ func _ready():
     State.connect("game_error", self, "_on_game_error")
     # Set the player name according to the system username. Fallback to the path.
     if OS.has_environment("USERNAME"):
-        $Connect/Name.text = OS.get_environment("USERNAME")
+        inputName.text = OS.get_environment("USERNAME")
     else:
         var desktop_path = OS.get_system_dir(0).replace("\\", "/").split("/")
-        $Connect/Name.text = desktop_path[desktop_path.size() - 2]
+        inputName.text = desktop_path[desktop_path.size() - 2]
 
 
 func _on_host_pressed():
-    if $Connect/Name.text == "":
-        $Connect/ErrorLabel.text = "Invalid name!"
+    if inputName.text == "":
+        lblError.text = "Invalid name!"
         return
 
-    $Connect.hide()
-    $Players.show()
-    $Connect/ErrorLabel.text = ""
+    panelConnect.hide()
+    panelPlayers.show()
+    lblError.text = ""
 
-    var player_name = $Connect/Name.text
+    var player_name = inputName.text
     State.host_game(player_name)
     refresh_lobby()
 
 
 func _on_join_pressed():
-    if $Connect/Name.text == "":
-        $Connect/ErrorLabel.text = "Invalid name!"
+    if inputName.text == "":
+        lblError.text = "Invalid name!"
         return
 
-    var ip = $Connect/IPAddress.text
-    if not ip.is_valid_ip_address():
-        $Connect/ErrorLabel.text = "Invalid IP address!"
-        return
+    lblError.text = ""
+    btnPlayOffline.disabled = true
+    btnPlayOnline.disabled = true
 
-    $Connect/ErrorLabel.text = ""
-    $Connect/Host.disabled = true
-    $Connect/Join.disabled = true
-
-    var player_name = $Connect/Name.text
+    var player_name = inputName.text
     State.join_game(ip, player_name)
 
 
 func _on_connection_success():
-    $Connect.hide()
-    $Players.show()
+    panelConnect.hide()
+    panelPlayers.show()
 
 
 func _on_connection_failed():
-    $Connect/Host.disabled = false
-    $Connect/Join.disabled = false
-    $Connect/ErrorLabel.set_text("Connection failed.")
+    btnPlayOffline.disabled = false
+    btnPlayOnline.disabled = false
+    lblError.set_text("Connection failed.")
 
 
 func _on_game_ended():
     show()
-    $Connect.show()
-    $Players.hide()
-    $Connect/Host.disabled = false
-    $Connect/Join.disabled = false
+    panelConnect.show()
+    panelPlayers.hide()
+    btnPlayOffline.disabled = false
+    btnPlayOnline.disabled = false
 
 
 func _on_game_error(errtxt):
     $ErrorDialog.dialog_text = errtxt
     $ErrorDialog.popup_centered_minsize()
-    $Connect/Host.disabled = false
-    $Connect/Join.disabled = false
+    btnPlayOffline.disabled = false
+    btnPlayOnline.disabled = false
 
 
 func refresh_lobby():
     var players = State.get_player_list()
     players.sort()
-    $Players/List.clear()
-    $Players/List.add_item(State.get_player_name() + " (You)")
+    listPlayers.clear()
+    listPlayers.add_item(State.get_player_name() + " (You)")
     for p in players:
-        $Players/List.add_item(p)
+        listPlayers.add_item(p)
 
-    $Players/Start.disabled = not get_tree().is_network_server()
-
+    btnStart.disabled = not get_tree().is_network_server()
 
 func _on_start_pressed():
     State.begin_game()
-
-
-func _on_find_public_ip_pressed():
-    OS.shell_open("https://icanhazip.com/")
