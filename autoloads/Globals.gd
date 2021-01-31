627b9ba33ad4bb8f53b65914a8fc04f65e382ba2
Angelo Aracri <angeloaracri@yahoo.de>
2021-01-31 06:49:07 +0100

    Prepare item category enum

--- autoloads/Globals.gd
+++ autoloads/Globals.gd
@@ -3,3 +3,30 @@ extends Node
 enum Orientation {
     N, E, S, W
 }
+
+enum ItemCategory {
+    Soft,
+    Food,
+    Dangerous,
+    Colorful,
+    Heavy,
+    KidsLoveIt,
+    Shiny,
+    Fun,
+    MyFavorite    
+}
+
+const ItemCategoryProbabilities = [
+    ItemCategory.Soft, ItemCategory.Soft, ItemCategory.Soft,
+    ItemCategory.Food, ItemCategory.Food, ItemCategory.Food,
+    ItemCategory.Dangerous, ItemCategory.Dangerous, ItemCategory.Dangerous,
+    ItemCategory.Colorful, ItemCategory.Colorful, ItemCategory.Colorful,
+    ItemCategory.Heavy, ItemCategory.Heavy, ItemCategory.Heavy,
+    ItemCategory.KidsLoveIt, ItemCategory.KidsLoveIt, ItemCategory.KidsLoveIt,
+    ItemCategory.Shiny, ItemCategory.Shiny, ItemCategory.Shiny,
+    ItemCategory.Fun, ItemCategory.Fun, ItemCategory.Fun,
+    ItemCategory.MyFavorite
+]
+
+func get_random_item_category():
+    return ItemCategoryProbabilities[Rand.randi() % ItemCategoryProbabilities.size()]
